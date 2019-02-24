dc8978ad17503da50969f37864765332f3652e1b
HienNgoc <ngochien.hien95@gmail.com>
2019-02-24 18:16:29 +0700

    model

---
+++ src/main/java/com/tma/entity/Employee.java
@@ -0,0 +1,100 @@
+package com.tma.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @Column(name = "employeenumber")
+    private Integer employeeNumber;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "firstname")
+    private String  firstName;
+
+    @Column(name = "extension")
+    private String extension;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "officecode")
+    private String officeCode;
+
+    @Column(name = "reportsto")
+    private String reportsTo;
+
+    @Column(name = "jobtitle")
+    private String jobTitle;
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOfficeCode() {
+        return officeCode;
+    }
+
+    public void setOfficeCode(String officeCode) {
+        this.officeCode = officeCode;
+    }
+
+    public String getReportsTo() {
+        return reportsTo;
+    }
+
+    public void setReportsTo(String reportsTo) {
+        this.reportsTo = reportsTo;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
